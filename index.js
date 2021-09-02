6899c2581ffe378699221c92d67eb710c3ecaea7
David Beitey <david@davidjb.com>
2021-09-02 17:31:17 +1000

    Demonstrate tableName lookup bug

--- index.js
+++ index.js
@@ -25,7 +25,13 @@ app.get("/", (req, res) => {
     res.setHeader("Content-Type", "text/html");
     res.write("<p>views: " + req.session.views + "</p>");
     res.write("<p>expires in: " + req.session.cookie.maxAge / 1000 + "s</p>");
-    res.end();
+
+    res.write("<h1>Tables</h1>");
+    res.write("<p>By default, the table name should be <a href=\"https://github.com/mweibel/connect-session-sequelize/blob/394645289e17c2e962fe6650729b3f0e691a3134/lib/connect-session-sequelize.js#L16\"><code>tableName: 'Sessions'</code></a>");
+    sequelize.query("SELECT name FROM sqlite_master WHERE type='table';").then(rows => {
+      res.write(`<div><code>${JSON.stringify(rows)}</code></div>`);
+      res.end();
+    });
   } else {
     req.session.views = 1;
     res.end("welcome to the session demo. refresh!");
